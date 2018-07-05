8f38d2ad7f43b24a0d28ffe7acc6ba7e32a8ab4e
jrforest <jrforest@ualberta.ca>
2018-07-05 15:34:18 -0600

    fix

--- key/static/passwordReset.js
+++ key/static/passwordReset.js
@@ -1,14 +1,14 @@
-function validateFormPasswordReset(thisForm){
-            with(thisForm){
-                var password = thisForm.password.value;
-                var password2 = thisForm.password2.value;
-                if (password == "" || password2 == ""){
-                    alert(("Passwords cannot be empty."));
-              return false;
-                }
-                if (password != password2) {
-              alert(("Passwords must match."));
-              return false;
-          }
-      }
-  }
+function validateFormPasswordReset(thisForm) {
+    with (thisForm) {
+        var password = thisForm.password.value;
+        var password2 = thisForm.password2.value;
+        if (password == "" || password2 == "") {
+            alert(("Passwords cannot be empty."));
+            return false;
+        }
+        if (password.valueOf().trim() != password2.valueOf().trim()) {
+            alert(("Passwords must match."));
+            return false;
+        }
+    }
+}
